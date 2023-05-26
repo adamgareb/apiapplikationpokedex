acf9f7369b9cb16b3c96412337b1d0bf27bc1128
adamgareb <92087000+adamgareb@users.noreply.github.com>
2023-05-27 00:31:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const pokedex = document.getElementById("pokedex");
+
+  console.log(pokedex);
+
+  const fetchPokemon = () => {
+
+    const promises = [];
+    for(let i = 1; i <= 151; i++){
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+
+  }
+
+    Promise.all(promises).then((results) => {
+      const pokemon = results.map((data) => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name).join(', ')
+      }));
+      displayPokemon(pokemon);
+    });
+  };
+
+  const displayPokemon = (pokemon) => {
+    console.log(pokemon); 
+    const pokemonHTMLString = pokemon.map ( pokemonn => `
+    <li class="card">
+      <img class="card-image" src="${pokemonn.image}" />
+      <h2 class="card-title">${pokemonn.id + "."}</h2>
+      <h2 class="card-title">${pokemonn.name}</h2>
+      <p class="card-subtitle">Type: ${pokemonn.type}</p>
+    </li>
+    `
+      )
+      .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+  }
+
+  fetchPokemon();
 }
 
 export default App;
